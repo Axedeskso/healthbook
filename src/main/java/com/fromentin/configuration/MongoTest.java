df47cc953bf21dd298dbcd65357a3a199df51a95
Fromentin <axfromenti@sfr.fr>
2017-11-15 13:57:36 +0100

    Revert "MongoTest"
    This reverts commit a7f87057c1043eee0bfbe4b3f666a7f60223d37c.

--- src/main/java/com/fromentin/configuration/MongoTest.java
+++ src/main/java/com/fromentin/configuration/MongoTest.java
@@ -13,70 +13,40 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
-import java.io.IOException;
 
 public class MongoTest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        String adress = "localhost";
-        int port = 27017;        
-        String bdd = "healthbook";
-        
         try {
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec("C://Program Files/MongoDB/Server/3.4/bin/mongod --dbpath C://Users/Axel/mongo/data/db");
-            System.out.println("Processus MongoDB lancé");
-            Mongo mongo = new Mongo(adress, port);
-            System.out.println("Connexion avec MongoDB établie sur "+adress+":"+port);
+            Mongo mongo = new Mongo("localhost", 27017);
 // GET DB
-            DB db = mongo.getDB(bdd);
-            System.out.println("Connexion avec la base "+bdd+" établie");
+            DB db = mongo.getDB("healthbook");
 // GET COLLECTION
-            DBCollection collectionUser = db.getCollection("user");
-            
-            System.out.println("Nettoyage de la collection USER en cours...");
-            DBCursor cr = collectionUser.find();
-            while (cr.hasNext()) {
-                collectionUser.remove(cr.next());
-            }
-            System.out.println("Nettoyage terminé");
-
+            DBCollection collection = db.getCollection("user");
 //INSERT documents with two fields id and datacol
-            BasicDBObject user1 = new BasicDBObject();
-            user1.put("username", "axedeskso");
-            user1.put("password", "31031993");
-            user1.put("role", "ADMINISTRATEUR");
-            user1.put("firstname", "Axel");
-            user1.put("surname", "FROMENTIN");
-            user1.put("dateNaissance", "31 mars 1993");
-            user1.put("lieuNaissance", "Villeneuve d'Ascq");
-            collectionUser.insert(user1);
+            BasicDBObject doc = new BasicDBObject();
+            doc.put("username", "axedeskso");
+            doc.put("password", "31031993");
+            doc.put("role", "ADMINISTRATEUR");
+            doc.put("firstname", "Axel");
+            doc.put("surname", "FROMENTIN");
+            doc.put("dateNaissance", "31 mars 1993");
+            doc.put("lieuNaissance", "Villeneuve d'Ascq");
+            collection.insert(doc);
 
-            BasicDBObject user2 = new BasicDBObject();
-            user2.put("username", "tlsIsisAndore");
-            user2.put("password", "123456789");
-            user2.put("role", "ADMINISTRATEUR");
-            user2.put("firstname", "Vincent");
-            user2.put("surname", "EYCHENNE");
-            user2.put("dateNaissance", "31 mars 1993");
-            user2.put("lieuNaissance", "Andorre");
-            collectionUser.insert(user2);
-            
-            BasicDBObject user3 = new BasicDBObject();
-            user3.put("username", "ctlg");
-            user3.put("password", "123456789");
-            user3.put("role", "ADMINISTRATEUR");
-            user3.put("firstname", "Pau");
-            user3.put("surname", "GARCIA GOZALVEZ");
-            user3.put("dateNaissance", "01-01-1990");
-            user3.put("lieuNaissance", "Barcelona");
-            collectionUser.insert(user3);
-
+            BasicDBObject doc2 = new BasicDBObject();
+            doc2.put("username", "tlsIsisAndore");
+            doc2.put("password", "123456789");
+            doc2.put("role", "ADMINISTRATEUR");
+            doc2.put("firstname", "Vincent");
+            doc2.put("surname", "EYCHENNE");
+            doc2.put("dateNaissance", "31 mars 1993");
+            doc2.put("lieuNaissance", "Andorre");
+            collection.insert(doc2);
 
 //GET all documents in collection
-            DBCursor cursor = collectionUser.find();
-            System.err.println("COLLECTION USER");
+            DBCursor cursor = collection.find();
             while (cursor.hasNext()) {
                 System.out.println(cursor.next());
             }
